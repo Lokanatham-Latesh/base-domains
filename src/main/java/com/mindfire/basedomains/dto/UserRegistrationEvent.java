0abc26502c352c090d3b8d6e3ce1b5f1679e5d78
Lokanatham Latesh <lokanathamlatesh2003@gmail.com>
2025-03-20 11:14:34 +0530

    initial commit

---
+++ src/main/java/com/mindfire/basedomains/dto/UserRegistrationEvent.java
@@ -0,0 +1,13 @@
+package com.mindfire.basedomains.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRegistrationEvent {
+    private String email;
+    private String verificationLink;
+}
